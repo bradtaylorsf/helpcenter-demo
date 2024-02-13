2dffb5608aace3c60665f46a5b90a74f3b453dd9
Brad Taylor <bradtaylorsf@gmail.com>
2024-02-13 13:43:43 -0800

    fixed typo in personas

--- src/lib/personas.js
+++ src/lib/personas.js
@@ -2,7 +2,7 @@ const personas = [
     { name: "Free User Frank", userPersona: "User", planType: "Free Plan", location: "North America" },
     { name: "Team Plan Tim", userPersona: "Developer", planType: "Team Plan", location: "North America" },
     { name: "Analytical Amy", userPersona: "Analyst", planType: "Enterprise Plan", location: "Europe" },
-    { name: "Admin Alex", userPersona: "Administrator", planType: "Enterprise Plan", location: "Asia Pacific" },
+    { name: "Admin Alex", userPersona: "Administrator", planType: "Enterprise Plan", location: "Asia-Pacific" },
     { name: "DevOps Dani", userPersona: "Developer", planType: "Enterprise Plan", location: "North America" },
 ];
 
